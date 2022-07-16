e8daef0108eda8145796b78da49ccd3635df1d5f
vsolanke <vsolanke@dexciss.com>
2022-07-16 11:37:48 +0530

    feat: Signature will appear from User Attachment

--- semi_dsc/sales_invoice.py
+++ semi_dsc/sales_invoice.py
@@ -3,11 +3,13 @@ import frappe
 
 @frappe.whitelist()
 def get_img_url(doc):
-    print(";;;;;;;;;;;",doc)
+    print(";;;;;;;;;;;",doc,frappe.session.user)
 
     query = frappe.db.sql("""select file_url from `tabFile`
-     where attached_to_doctype = '{0}' and attached_to_name ='{1}' 
+     where attached_to_doctype = 'User' and attached_to_name ='{0}' 
      and file_name like 'sign%' or file_name like 'Sign%' or file_name like 'SIGN%' limit 1
-     """.format(doc,doc.name),as_dict=1)
+     """.format(frappe.session.user),as_dict=1)
     for i in query:
         return i.get("file_url")
+
+        
